43670a254c148705365439e2e1274efce897544e
Wenqian Li <91103791+wel013@users.noreply.github.com>
2024-07-04 10:29:47 -0400

    Update main.py

--- Intermediate/WebCapstoneBlog/main.py
+++ Intermediate/WebCapstoneBlog/main.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request
 import requests
 import datetime
 import csv
-# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)

be4a8edaaf82a2b7cb2a311120cb939c025a9dc6
Wenqian Li <91103791+wel013@users.noreply.github.com>
2024-07-08 12:41:11 -0400

    Update main.py

--- Advanced/FullyUpdatedBlog/main.py
+++ Advanced/FullyUpdatedBlog/main.py
@@ -13,6 +13,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import hashlib
 from urllib.parse import urlencode
+import os
 
 '''
 Make sure the required packages are installed:
@@ -28,12 +29,12 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-
 
 
 # CREATE DATABASE
@@ -41,7 +42,8 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -121,6 +123,7 @@ gravatar = Gravatar(app,
                     force_lower=False,
                     use_ssl=False,
                     base_url=None)
+
 
 @app.route('/register', methods=["POST", "GET"])
 def register():
@@ -262,4 +265,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False)

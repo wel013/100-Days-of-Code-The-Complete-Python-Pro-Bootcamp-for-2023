f423c3712d2f8bb3a629a312832bc3f4d9be6ffc
Wenqian Li <91103791+wel013@users.noreply.github.com>
2024-07-19 12:48:12 -0400

    Add files via upload

---
+++ ProfessionalPortfolio/MorseCodeConverter/local_trans.py
@@ -0,0 +1,25 @@
+# this dict is from online source: https://www.geeksforgeeks.org/morse-code-translator-python/#
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+res = ''
+text = input("Please enter what you want to translate into morse code:").upper()
+for letter in text:
+    if letter in MORSE_CODE_DICT:
+        res += MORSE_CODE_DICT[letter]
+        res += " "
+    else:
+        res = "Sorry we cannot find some of the characters. We will keep updating to ensure better experience!"
+print(res)

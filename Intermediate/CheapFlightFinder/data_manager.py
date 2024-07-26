5840b6fffe222b5a857ff2edf9e9a7ef79588b47
Wenqian Li <91103791+wel013@users.noreply.github.com>
2024-07-26 16:54:38 -0400

    Retrieves customer number

--- Intermediate/CheapFlightFinder/data_manager.py
+++ Intermediate/CheapFlightFinder/data_manager.py
@@ -12,19 +12,14 @@ class DataManager:
 
     def __init__(self):
         self.destination_data = {}
+        self.customer_number_list = []
 
     def get_destination_data(self):
-        # 2. Use the Sheety API to GET all the data in that sheet and print it out.
         response = requests.get(url=SHEETY_PRICES_ENDPOINT)
         data = response.json()
         self.destination_data = data["prices"]
-        # 3. Try importing pretty print and printing the data out again using pprint().
         # pprint(data)
         return self.destination_data
-
-    # 6. In the DataManager Class make a PUT request and use the row id  from sheet_data
-    # to update the Google Sheet with the IATA codes. (Do this using code).
-
     def update_destination_codes(self, row):
         print("in updataing code")
         put_url = f"<insert your url>"
@@ -40,8 +35,8 @@ class DataManager:
         )
         print(response.text)
 
-    def update_price(self, id, price):
-        put_url = f"<insert your url>"
+ def update_price(self, id, price):
+        put_url = <insert yout endpoint>
         # for city in self.destination_data:
         new_data = {
             "price": {
@@ -54,4 +49,12 @@ class DataManager:
         )
         print(response.text)
 
+    def get_customer_number(self):
+        customers_endpoint = <insert your endpoint>
+        response = requests.get(customers_endpoint)
+        data = response.json()
+        print(data)
+        self.customer_number_list = [
+            user['whatIsYourNumber?'] for user in data['users']]
+        return self.customer_number_list
 

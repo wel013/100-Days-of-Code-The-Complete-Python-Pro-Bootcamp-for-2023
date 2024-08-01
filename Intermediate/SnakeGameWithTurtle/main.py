e6c374f3752b46a45f0e00d69af135a7646d8887
Wenqian Li <91103791+wel013@users.noreply.github.com>
2024-08-01 16:57:24 -0400

    Update main.py

--- Intermediate/SnakeGameWithTurtle/main.py
+++ Intermediate/SnakeGameWithTurtle/main.py
@@ -1,7 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
-
+from food import Food
+from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -11,11 +12,12 @@ screen.listen()
 
 
 snake = Snake(20)
+food = Food()
+scoreboard = Scoreboard()
+
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
-
 screen.onkey(snake.left, "Left")
-
 screen.onkey(snake.right, "Right")
 game_is_on = True
 
@@ -24,4 +26,18 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.snake_move()
+    # detect collision with food
+    if snake.segments[0].distance(x=food) < 15:
+        snake.grow()
+        food.shift_new_loc()
+        scoreboard.gain_points()
+    if snake.segments[0].xcor() > 280 or snake.segments[0].xcor() < -280 or snake.segments[0].ycor() > 280 or snake.segments[0].ycor() < -280:
+        game_is_on = False
+        scoreboard.gameover()
+    # detect if head collide with any of the segments
+    for seg in snake.segments[1:]:
+        if snake.segments[0].distance(seg) < 10:
+            game_is_on = False
+            scoreboard.gameover()
+
 screen.exitonclick()

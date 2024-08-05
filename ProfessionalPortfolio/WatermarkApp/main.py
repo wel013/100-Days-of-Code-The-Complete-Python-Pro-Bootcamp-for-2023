14a55c0cbf35b7d67edb41843005da64250b9eff
Wenqian Li <91103791+wel013@users.noreply.github.com>
2024-08-05 17:45:58 -0400

    need to find font size and make make text bigger

---
+++ ProfessionalPortfolio/WatermarkApp/main.py
@@ -0,0 +1,16 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+with Image.open("./images/Capture.png") as img:
+    img = img.resize((800, 600))  # Adjust the size as needed
+    img.save("./images/Capture_resized.png")
+
+
+font_path = 'Roboto-Regular.ttf'
+font_size = 72
+myFont = ImageFont.load_default()
+img = Image.open('./images/Capture_resized.png')
+I1 = ImageDraw.Draw(img)
+I1.text((400, 300),  "Watermark!!", font=myFont, fill=(0, 0, 0))
+img.show()
+# img.save("./images/Capture_watermarked.png")
